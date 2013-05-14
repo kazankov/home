b339a650acd558e9a5da4bfcaddae53c9ed7bfa5
kazankov <kazankov@quickline.ru>
2013-05-14 06:17:41 -0700

    добавил создание индексов

--- tamgdenas/pg2mongo.py
+++ tamgdenas/pg2mongo.py
@@ -10,7 +10,11 @@ import sys
 conn = psycopg2.connect("dbname='poi' user='postgres' host='localhost' password='postgres'",connection_factory=psycopg2.extras.DictConnection)
 
 mConn = Connection()
+#**** base creation
 #mConn.drop_database("poi")
+#mConn.poi.poi.ensure_index('sourceId');
+#mConn.poi.poi.ensure_index('geoPoint', '2d');
+#******************
 mPoi = mConn.poi.poi
 mPoiTypes = mConn.poi.poiTypes
 mReviews = mConn.poi.reviews
